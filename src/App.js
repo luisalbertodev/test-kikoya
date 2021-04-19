7f27903cdda6cdc88bf01d24689d59539a89912c
Héctor Arturo Carranza <arturo_luc2@hotmail.com>
2021-04-19 12:29:03 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Cart from './containers/Cart';
+import ProductList from './containers/ProductList';
+
+const App = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1>Virtual Shop</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-8">
+                    <ProductList />
+                </div>
+                <div className="col-md-4">
+                    <Cart />
+                </div>
+            </div>
+
+            <footer>
+                <small>
+                    powered by <a href="http://www.kikoya.mx/">Kikoya</a>
+                </small>
+            </footer>
+        </div>
+    );
+}
+
+export default App;
